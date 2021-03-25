1433e783de6be8da48324df29823e8f898463de2
Gishan <gruw0001@student.monash.edu>
2021-03-25 17:04:28 +1100

    Updated motor test for channel 1 and 4

--- motortest/MotorTest.ino
+++ motortest/MotorTest.ino
@@ -4,52 +4,66 @@
 
 #include <AFMotor.h>
 
-AF_DCMotor motor(4;
+AF_DCMotor motor4(4);
+AF_DCMotor motor1(1, MOTOR12_64KHZ);
 
-void setup() {
+void setup()
+{
   Serial.begin(9600); // set up Serial library at 9600 bps
   Serial.println("Motor test!");
 
   // turn on motor
-  motor.setSpeed(200);
+  motor4.setSpeed(200);
+  motor1.setSpeed(200);
 
-  motor.run(RELEASE);
+  motor4.run(RELEASE);
+  motor1.run(RELEASE);
 }
 
-void loop() {
+void loop()
+{
   uint8_t i;
 
   Serial.print("tick");
 
-  motor.run(FORWARD);
+  motor4.run(FORWARD);
+  motor1.run(FORWARD);
   for (i = 0; i < 255; i++)
   {
-    motor.setSpeed(i);
+    Serial.println("Forward");
+    motor4.setSpeed(i);
+    motor1.setSpeed(i);
     delay(10);
   }
 
   for (i = 255; i != 0; i--)
   {
-    motor.setSpeed(i);
+    motor4.setSpeed(i);
+    motor1.setSpeed(i);
     delay(10);
   }
 
   Serial.print("tock");
 
-  motor.run(BACKWARD);
+  motor4.run(BACKWARD);
+  motor1.run(BACKWARD);
   for (i = 0; i < 255; i++)
   {
-    motor.setSpeed(i);
+    Serial.println("Backward");
+    motor4.setSpeed(i);
+    motor1.setSpeed(i);
     delay(10);
   }
 
   for (i = 255; i != 0; i--)
   {
-    motor.setSpeed(i);
+    motor4.setSpeed(i);
+    motor1.setSpeed(i);
     delay(10);
   }
 
   Serial.print("tech");
-  motor.run(RELEASE);
+  motor4.run(RELEASE);
+  motor1.run(RELEASE);
   delay(1000);
 }
